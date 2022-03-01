34c8957d85bd9e226d86b84cc908aad4a484f11e
George <georgesucuzhanay100@gmail.com>
2022-02-28 21:57:34 -0500

    finished maping posts to post page with redux

--- src/pages/PostsPage.js
+++ src/pages/PostsPage.js
@@ -1,8 +1,39 @@
-const PostsPage = () => {
-   return(
-       <section>
-           <h1>Posts</h1>
-       </section>
-   )
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions/postsActions";
+import { useEffect } from "react";
+import { Post } from "../components/Post";
+
+
+// Redux State is now in the props of the component
+const PostsPage = ({dispatch, loading, posts, hasErrors}) => {
+    useEffect(() => {
+        dispatch(fetchPosts())
+    },  [dispatch])
+
+
+    // show loading, error or success state
+    const renderPosts = () => {
+        if (loading) return <p>Loading posts...</p>
+        if (hasErrors) return <p>Unable to display posts.</p>
+        return posts.map((post) => <Post key={post.id} post={post} />)
+    }
+
+   
+    return(
+        <section>
+            <h1>Posts</h1>
+            {renderPosts()}
+        </section>
+    )
 }
-export default PostsPage;
+
+ // Map Redux state to React component prop
+ const mapStateToProps = (state) => ({
+    loading: state.posts.loading,
+    posts: state.posts.posts,
+    hasErrors: state.posts.hasErrors,
+})
+
+
+
+export default connect(mapStateToProps)(PostsPage);
